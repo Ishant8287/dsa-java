7861d517a0cc1b7fac298a5b5dd722dfdcfe91b0
Ishant Singh <singhishant683@gmail.com>
2026-03-26 12:23:05 +0530

    feat : I learn pre in post concept and solve tower of hanoi problem

--- recursion/Level_2/GenerateParenthesis.java
+++ recursion/Level_2/GenerateParenthesis.java
@@ -1,7 +1,28 @@
 package Level_2;
+
+import java.util.*;
+
+
 //Lc 22
 public class GenerateParenthesis {
     static void main() {
+        int n = 1;
+        List<String> ans = new ArrayList<>();
+        helper("", 0, 0, ans, n);
+        System.out.print(ans);
 
+    }
+
+    static void helper(String ans, int l, int r, List<String> list, int x) {
+        if (r == x) {
+            list.add(ans);
+            return;
+        }
+        if (l < x) {
+            helper(ans + "(", l + 1, r, list, x);
+        }
+        if (r < l) {
+            helper(ans + ")", l, r + 1, list, x);
+        }
     }
 }
--- recursion/Level_2/TowerOfHanoi.java
+++ recursion/Level_2/TowerOfHanoi.java
@@ -2,6 +2,20 @@ package Level_2;
 
 public class TowerOfHanoi {
     static void main() {
+        hanoi(6, 'A', 'B', 'C');
+    }
+
+    static void hanoi(int n, char source, char helper, char destination) {
+        if (n == 0) return;
+
+        //n-1 disks from source to helper via destination
+        hanoi(n - 1, source, destination, helper);
+
+        //Print-> largest from source to destination
+        System.out.println(source + "->" + destination);
+
+        //n-1 disk from helper to destination via source
+        hanoi(n - 1, helper, source, destination);
 
     }
 }
